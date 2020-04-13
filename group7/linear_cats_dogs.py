4289b26fe9c07f3aa1c5064e56e7ebe9b24d6933
Mohsan Tahir <mohsan.tahir@yahoo.com>
2020-04-13 16:18:19 +0200

    small changes

--- group7/linear_cats_dogs.py
+++ group7/linear_cats_dogs.py
@@ -8,6 +8,8 @@ from dlvc.ops import *
 from dlvc.datasets.pets import PetsDataset
 from dlvc.batches import BatchGenerator
 from dlvc.dataset import Subset
+import matplotlib.pyplot as plt
+from mpl_toolkits import mplot3d
 
 TrainedModel = namedtuple('TrainedModel', ['model', 'accuracy'])
 
@@ -82,17 +84,64 @@ def train_model(lr: float, momentum: float) -> TrainedModel:
 lr, m = random_search()
 baseline = train_model(lr = lr, momentum = m)
     
-
+all_models = list()
 best_model = baseline
+all_models.append(baseline)
 acc_threshold = 0.9
-max_iter = 100
+max_iter = 9
 
 for i in np.arange(max_iter):
     lr, m = random_search()
     clfAccuracy = train_model(lr = lr, momentum = m)
+    all_models.append(clfAccuracy)
     if clfAccuracy.accuracy > best_model.accuracy:
         best_model = clfAccuracy
     if clfAccuracy.accuracy.accuracy() >= 0.9:
         best_model = clfAccuracy
         break
 print(best_model.accuracy)
+
+fig = plt.figure(figsize=(20,15))
+ax = plt.axes(projection="3d")
+
+#making a 3d plot for the hyperparameters
+num_bars = len(all_models)
+x_pos = list()
+y_pos = list()
+z_size = list()
+
+for model in all_models:
+    x_pos.append(model.model.lr)
+    y_pos.append(model.model.momentum)
+    z_size.append(model.accuracy.accuracy())
+
+#print(x_pos)
+z_pos = np.zeros(num_bars)
+x_size = np.repeat(0.02, num_bars)
+y_size = np.repeat(0.02, num_bars)
+
+ax.bar3d(x_pos, y_pos, z_pos, x_size, y_size, z_size, color='red')
+ax.set_xlabel('learning rate')
+ax.set_ylabel('momentum')
+ax.set_zlabel('accuracy')
+plt.show()
+
+#test accuracy:
+
+
+accuracy = Accuracy()
+    
+test_batches = iter(bg_test)
+j = 1
+for batch in test_batches:
+    
+    if(j < len(bg_test)):
+        predictions = best_model.model.predict(data=batch.data)
+        target = batch.label
+        accuracy.update(predictions, target)
+        j += 1
+        
+    else:
+        break
+
+print(f"Accuracy on test set of best model: {accuracy}")
