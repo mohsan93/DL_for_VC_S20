356a486fc83f5a87a771499d16be2802ddd48b43
Mohsan Tahir <mohsan.tahir@yahoo.com>
2020-04-13 14:23:18 +0200

    added finished linear_cats_dogs.py

--- group7/linear_cats_dogs.py
+++ group7/linear_cats_dogs.py
@@ -3,18 +3,32 @@ from collections import namedtuple
 from dlvc.models.linear import LinearClassifier
 from dlvc.test import Accuracy
 
+import numpy as np
+from dlvc.ops import *
+from dlvc.datasets.pets import PetsDataset
+from dlvc.batches import BatchGenerator
+from dlvc.dataset import Subset
+
 TrainedModel = namedtuple('TrainedModel', ['model', 'accuracy'])
 
 # TODO implement steps 1-2
 
 
-#trainingDataset = PetsDataset("D:\\fuck\\dlvc2020\\assignments\\cifar-10-batches-py", Subset.TRAINING)
-#validationDataset = PetsDataset("D:\\fuck\\dlvc2020\\assignments\\cifar-10-batches-py", Subset.VALIDATION)
-#testDataset = PetsDataset("D:\\fuck\\dlvc2020\\assignments\\cifar-10-batches-py", Subset.TEST)
+trainingDataset = PetsDataset("D:\\fuck\\dlvc2020\\assignments\\cifar-10-batches-py", Subset.TRAINING)
+validationDataset = PetsDataset("D:\\fuck\\dlvc2020\\assignments\\cifar-10-batches-py", Subset.VALIDATION)
+testDataset = PetsDataset("D:\\fuck\\dlvc2020\\assignments\\cifar-10-batches-py", Subset.TEST)
 
-#bg_training = BatchGenerator(dataset=trainingDataset, num=500, shuffle=True, op=op)
-#bg_validation = BatchGenerator(dataset=validationDataset, num=500, shuffle=True, op=op)
-#bg_test = BatchGenerator(dataset=testDataset, num=500, shuffle=True, op=op)
+op = chain([
+    vectorize(),
+    type_cast(np.float32),
+    add(-127.5),
+    mul(1/127.5),
+])
+
+
+bg_training = BatchGenerator(dataset=trainingDataset, num=500, shuffle=True, op=op)
+bg_validation = BatchGenerator(dataset=validationDataset, num=500, shuffle=True, op=op)
+bg_test = BatchGenerator(dataset=testDataset, num=500, shuffle=True, op=op)
 
 
 def random_search(lr_max=1, lr_min=0.5, momentum_max=1, momentum_min=0):
@@ -35,8 +49,9 @@ def train_model(lr: float, momentum: float) -> TrainedModel:
     n_epochs = 10
     for i in range(n_epochs):
         train_batches = iter(bg_training)
-        i=0
+        i=1
         for batch in train_batches:
+            
             if(i<len(bg_training)):
                 clf.train(data= batch.data, labels = batch.label)
                 i += 1
@@ -46,16 +61,37 @@ def train_model(lr: float, momentum: float) -> TrainedModel:
     accuracy = Accuracy()
     
     val_batches = iter(bg_validation)
-    j = 0
+    j = 1
     for batch in val_batches:
+        
         if(j < len(bg_validation)):
             predictions = clf.predict(data=batch.data)
             target = batch.label
             accuracy.update(predictions, target)
             j += 1
+            
         else:
             break
 
     return TrainedModel(clf, accuracy)
 
 # TODO implement steps 4-7
+
+#random baseline accuracy:
+
+lr, m = random_search()
+baseline = train_model(lr = lr, momentum = m)
+    
+
+best_model = baseline
+acc_threshold = 0.9
+max_iter = 100
+
+for i in np.arange(max_iter):
+    lr, m = random_search()
+    clfAccuracy = train_model(lr = lr, momentum = m)
+    if clfAccuracy.accuracy > best_model.accuracy:
+        best_model = clfAccuracy
+    if clfAccuracy.accuracy.accuracy() >= 0.9:
+        best_model = clfAccuracy
+        break
